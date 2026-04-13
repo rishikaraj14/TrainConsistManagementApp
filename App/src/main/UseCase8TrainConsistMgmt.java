d712f62d40c9cb20a510557f456aa74094cb12cd
Rishika Raj <rishikaraj609@gmail.com>
2026-04-13 15:46:12 +0530

    RISHIKA RAJ ADDED UC8

--- App/src/main/UseCase8TrainConsistMgmt.java
+++ App/src/main/UseCase8TrainConsistMgmt.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
  * Demonstrates Stream API filtering based on capacity
  * without modifying the original collection.
  *
- * Author: Vineet Seth
+ * Author: RISHIKA RAJ
  * Version: 8.0
  */
 
